b60460ac15884042b0db9e2fe36fcd9fc03261bb
Federico Fuga <fuga@studiofuga.com>
2024-11-30 18:23:27 +0100

    First integration of tg bot

--- src/tg_bot.rs
+++ src/tg_bot.rs
@@ -1,8 +1,13 @@
+use bsky_sdk::api::com::atproto::sync::subscribe_repos::Message;
+use teloxide::Bot;
+use teloxide::prelude::*;
+use teloxide::utils::command::BotCommands;
 use tokio::sync::mpsc;
 use tokio::sync::mpsc::{Receiver, Sender};
 use crate::feed::FeedEntry;
 
 pub struct telegram_bot {
+    bot: Bot,
     tx: Sender<Action>,
     rx: Receiver<Action>
 }
@@ -11,10 +16,19 @@ pub enum Action {
     NewFeeds{ feeds: Vec<FeedEntry>}
 }
 
+#[derive(BotCommands, Clone)]
+#[command(rename_rule = "lowercase")]
+enum Command {
+    Start
+}
+
 impl telegram_bot {
-    pub fn build(key: &str) -> telegram_bot {
+    pub async fn build(key: &str) -> telegram_bot {
         let (tx, rx) = mpsc::channel(32);
+        let bot = Bot::from_env();
+
         telegram_bot{
+            bot,
             tx,
             rx
         }
@@ -23,4 +37,14 @@ impl telegram_bot {
     pub fn channel(&self) -> Sender<Action> {
         self.tx.clone()
     }
+
+    pub async fn start(&self) {
+        // NO. Use Dispatcher instead
+        Command::repl(&self.bot, bot_reply).await;
+    }
+
+}
+
+async fn bot_reply(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    Ok(())
 }
